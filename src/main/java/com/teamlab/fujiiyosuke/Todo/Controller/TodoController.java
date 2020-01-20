e85835221e6026ef522e0ee543ddde0e83eb4774
touyou <fujiyou1121@gmail.com>
2020-01-20 16:21:27 +0900

    modify controller structure

--- src/main/java/com/teamlab/fujiiyosuke/Todo/Controller/TodoController.java
+++ src/main/java/com/teamlab/fujiiyosuke/Todo/Controller/TodoController.java
@@ -1,11 +1,21 @@
-package com.teamlab.fujiiyosuke.Todo;
+package com.teamlab.fujiiyosuke.Todo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SearchController {
+public class TodoController {
+
+    @GetMapping("/")
+    public String top(Model model) {
+        return "top";
+    }
+
+    @GetMapping("/edit")
+    public String edit(Model model) {
+        return "edit";
+    }
 
     @GetMapping("/search")
     public String search(Model model) {
