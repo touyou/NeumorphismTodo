68aa2d369ac99ca41c082bdfd208d932c58aafc4
touyou <fujiyou1121@gmail.com>
2020-01-21 14:33:57 +0900

    add Deadline annotation

---
+++ src/main/java/com/teamlab/fujiiyosuke/todoApp/constraints/Deadline.java
@@ -0,0 +1,28 @@
+package com.teamlab.fujiiyosuke.todoApp.constraints;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotNull;
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = {})
+@ReportAsSingleViolation
+@NotNull
+public @interface Deadline {
+    String message() default "{com.teamlab.fujiiyosuke.validator.constraints.Deadline.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    public static @interface List {
+        Deadline[] value();
+    }
+}
